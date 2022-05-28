23e528115b50d57e61c5a84dc204a0612a20db47
unknown <you@example.com>
2022-05-28 12:22:57 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# check if two words are anagrams
+# Example:
+# find_anagrams("hello","check")-->False
+# find_anagrams("below"'"elbow")-->True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+
+    if len(word) != len(anagram):
+        return False
+
+    word = sorted(word)
+    anagram = sorted(anagram)
+
+    if (word == anagram):
+        return True
+    return False
+        
+
+
+print(find_anagram("hello", "check"))
+print(find_anagram("below", "elbow"))
